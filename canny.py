e2ae7a2aa422865f24d371ffa45e5eddca93353e
Abraham <43353411+Rezatachi@users.noreply.github.com>
2023-05-03 00:58:59 -0700

    Update canny.py
    Canny Script (should work if we have perms)

--- canny.py
+++ canny.py
@@ -1,23 +1,64 @@
-import os 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 import cv2
 import shutil
-import pandas as pd
+from skimage.io import imread
+from skimage.transform import resize
+from skimage.feature import hog
+from skimage import exposure
 
-def demo_canny_test(filename):
-    im = cv2.imread(filename=filename)
-    im = cv2.resize(im, (224,224))
-    # convert to grayscale and add gauss
-    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    # gauss = cv2.GaussianBlur(im_gray, (5, 5), 0)
-    # locate a threshold
-    _, threshold = cv2.threshold(im_gray, 130, 190, 0)
-    # edges = cv2.Canny(threshold, 100, 255)
-    plt.figure()
-    plt.imshow(threshold, cmap="gray"), #plt.xticks([]), plt.yticks([])  
+def get_folder(folder):
+    # get folder, just to test this function
+    # ignore this function
+    for root, dirs, files in os.walk(folder):
+        if dirs != []:
+            print(dirs)
+        else:
+            print(root)
 
+def canny(folder, desired_path):
+    os.chdir(folder)
+    # actual hog work
+    for root, directory, files in os.walk(folder):
+        if directory == []:
+            for file in files:
+                d = direc + "/" if direc != [] else ""
+                filename = root + "/" + d + file
+                assert os.path.exists(filename)
+                im = cv2.imread(filename)
+                im = cv2.resize(im, (224,224))
+                im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                im_gray = cv2.GaussianBlur(im_gray, (5,5), 0)
+                _, threshold = cv2.threshold(im_gray, 130, 190, 0)
+                edges = cv2.Canny(threshold, 100, 255)
+                cv2.imwrite(file, edges)
+                shutil.move(file, f"{desired_path}/{root}/{d}")
+        else:
+            for dirs in directory:
+                for root, direc, files, in os.walk(dirs):
+                    for file in files:
+                        d = direc + "/" if direc != [] else ""
+                        filename = root + "/" + d + file
+                        assert os.path.exists(filename)
+                        im = cv2.imread(filename)
+                        im = cv2.resize(im, (224,224))
+                        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                        im_gray = cv2.GaussianBlur(im_gray, (5,5), 0)
+                        _, threshold = cv2.threshold(im_gray, 130, 190, 0)
+                        edges = cv2.Canny(threshold, 100, 255)
+                        cv2.imwrite(file, edges)
+                        shutil.move(file, f"{desired_path}/{root}/{d}")
+                        
 
-    plt.show()
-
-demo_canny_test("view3_frontal.jpg")
+# make a new directory for train and test
+canny_train_path = "canny-detecion-train"
+canny_test_path = "canny-detecion-test"
+os.mkdir(canny_train_path)
+os.mkdir(canny_test_path)
+# you can run these commands in your own workspace and use the images in your models.
+folder_path_train = "/groups/CS156b/data/train"
+folder_path_test = "/groups/CS156b/data/test"
+canny(folder_path, hog_train_path)
+canny(folderpath, hog_test_path)
